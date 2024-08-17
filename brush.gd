413960eb01adeb09aa68b8da26db523f0a023ca9
knuklz <a.monaghan90@gmail.com>
2024-08-17 18:39:21 +1000

    Centered Brush on scaled sprite
    Brush now centered on sprite and scales appropriately.
    
    Colour test on a basic level, commented out to not conflict with other branch.

--- brush.gd
+++ brush.gd
@@ -1,11 +1,15 @@
 extends Sprite2D
 
-
+@export var min:int #= 50
+@export var max:int #= 200
 #$Line2D
-var xSP: int = randi_range(10,200)
-var ySP: int = randi_range(10,200)
+@onready var xSP: int = randi_range(min,max)
+@onready var ySP: int = randi_range(min,max)
 
-@export var speed: Vector2 = Vector2(xSP,ySP)
+@onready var speed: Vector2 = Vector2(xSP,ySP)
+
+#@export var tr_Col: Color = Color(randf_range(.2,.8),randf_range(.2,.7),randf_range(.2,.8),.1)
+
 # Get the height and width of the sprite for checking boundaries
 var height: Vector2 = texture.get_size()
 #var width: int = Texture.get_width()
@@ -18,14 +22,18 @@ var wWidth: int = 500
 
 # Variable to store the trail
 @onready var trail: Line2D = $Line2D
-
-
+@onready var world: Node2D = null
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	#print($".".scale)
-	pass # Replace with function body.
+	#trail.default_color = tr_Col
+	trail.position = Vector2((height.x * scale.x)/2,(height.y * scale.x)/2)
+	world = get_parent()
+	wHeight = world.WORLD_HEIGHT
+	wWidth = world.WORLD_WIDTH
+	#pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -34,10 +42,10 @@ func _process(delta: float) -> void:
 	trail.add_point(position)
 	
 	
-	if((position.x >=  (wWidth-height.x) and (speed.x > 0)) or (position.x<=0) and speed.x <0):
+	if((position.x >=  (wWidth-(height.x * scale.x)) and (speed.x > 0)) or (position.x<=0) and speed.x <0):
 		speed.x = speed.x * -1
 		
-	if((position.y >=  (wHeight-height.x) and (speed.y > 0)) or (position.y<=0) and speed.y <0):
+	if((position.y >=  (wHeight-(height.y * scale.x)) and (speed.y > 0)) or (position.y<=0) and speed.y <0):
 		speed.y = speed.y * -1
 	# Update the X position based on the speed variable  
 	position += speed*delta
--- main.gd
+++ main.gd
@@ -1,10 +1,11 @@
 extends Node2D
 
-@export var WORLD_HEIGHT: int = 500
-@export var WORLD_WIDTH : int = 500
-
+@export var WORLD_HEIGHT: int = 500 #size.y
+@export var WORLD_WIDTH : int = 500 # size.x
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	#WORLD_HEIGHT= get_window().size.x
+	
 	pass # Replace with function body.
 
 
